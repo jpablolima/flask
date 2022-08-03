607f7730758f442554da701d0b00c28b6f17514c
João Pablo <jp.pablolima@gmail.com>
2022-08-03 17:21:49 -0300

    página lista de jogos

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+
+@app.route("/jogos")
+def jogos():
+    jogo1 = Jogo("God of War", "Hack n Slash", "PS3")
+    jogo2 = Jogo("Skyrim", "RPG", "PC")
+    jogo3 = Jogo("Chrono Trigger", "RPG", "Nintendo DS")
+
+    jogos = [jogo1, jogo2, jogo3]
+
+    return render_template("jogos.html", titulo="Jogos", litaJogos=jogos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run(debug = True)
