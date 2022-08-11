8266b3eed1576095e5b12260ff56b82ab8a2c850
João Pablo <jp.pablolima@gmail.com>
2022-08-11 18:06:33 -0300

    configuração de autenticação

--- app.py
+++ app.py
@@ -4,22 +4,26 @@ import re
 from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
-app.secret_key='gandalf'
+app.secret_key = 'gandalf'
+
 
 class Jogo:
     def __init__(self, nome, categoria, console):
-        self.nome=nome
-        self.categoria=categoria
-        self.console=console
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 
 jogo1 = Jogo("God of War", "Hack n Slash", "PS3")
 jogo2 = Jogo("Skyrim", "RPG", "PC")
 jogo3 = Jogo("Chrono Trigger", "RPG", "Nintendo DS")
 lista = [jogo1, jogo2, jogo3]
 
+
 @app.route('/home')
 def home():
     return render_template("index.html", titulo="Games")
+
 
 @app.route("/jogos")
 def jogos():
@@ -28,7 +32,10 @@ def jogos():
 
 @app.route("/novojogo")
 def novojogo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novojogo.html', titulo="Novo Jogo")
+
 
 @app.route("/criarjogo", methods=['POST'])
 def criarjogo():
@@ -43,19 +50,26 @@ def criarjogo():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
     if 'bazinga' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
-        flash(session['usuario_logado'] + 'logado com sucesso!')
-        return redirect('/jogos')
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        proxima_pagina = request.form['proxima']
+        return redirect('/jogos{}'.format(proxima_pagina))
+        # return redirect('/jogos')
     else:
         flash('Erro no login!')
         return redirect('/login')
-
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/home')
 
 
 app.run(debug=True)
