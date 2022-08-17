45248ad9bd3f54118726c244ee64e23e1e142f18
João Pablo <jp.pablolima@gmail.com>
2022-08-17 11:52:11 -0300

    listando jogos

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+
+
 app = Flask(__name__)
 
 
@@ -10,7 +18,11 @@ def home():
 
 @app.route('/inicio')
 def hello_world():
-    return render_template('jogos.html', titulo='Jogos')
+    jogo1 = Jogo('God of War','Hack n slash', 'PS5')
+    jogo2 = Jogo('Skyrim','RPG', 'PC')
+    jogo3 = Jogo('PES','Esporte', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('jogos.html', titulo='Jogos', jogos=lista)
 
 
 app.run(debug=True)
