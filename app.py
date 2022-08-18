8f00f20311528bc632a10c2f0f6ce21144225212
João Pablo <jp.pablolima@gmail.com>
2022-08-18 18:05:51 -0300

    metodo post no servidor

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,6 +7,10 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('God of War','Hack n slash', 'PS5')
+jogo2 = Jogo('Skyrim','RPG', 'PC')
+jogo3 = Jogo('PES','Esporte', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 
 app = Flask(__name__)
@@ -16,13 +21,22 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/inicio')
-def hello_world():
-    jogo1 = Jogo('God of War','Hack n slash', 'PS5')
-    jogo2 = Jogo('Skyrim','RPG', 'PC')
-    jogo3 = Jogo('PES','Esporte', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
-    return render_template('jogos.html', titulo='Jogos', jogos=lista)
+@app.route('/jogos')
+def lista_jogos():
+    
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run(debug=True)
